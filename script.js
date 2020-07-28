796573d4801630ac4b6e418b0cdd9740d00839c7
dorj222 <dorj222@gmail.com>
2020-07-27 23:13:28 -0400

    Updated the UI; added about me page and Selected projects page.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
-const btnToggle = document.getElementById('btnToggle');
-
-//Toggle
-btnToggle.addEventListener("click", ()=>
-    document.body.classList.toggle('show-nav')
-);
+buttons = document.getElementsByClassName('selectBtn');
 
 
 
+//Hover effect
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mouseover", function(event) {   
+
+     event.target.style.color = "white";
+
+    setTimeout(function() {
+            event.target.style.color = ""; }, 500); }, false);
+
+}
+           
